ac8c5d6053f5cf0cd3ec164664da942de04e2b5c
liambarden <liamofbarden@googlemail.com>
2013-10-10 15:27:46 +0100

    attempting to load opencv

---
+++ Project/obj/local/armeabi/objs/opencv_java/opencv_java.o.d
@@ -0,0 +1,4 @@
+obj/local/armeabi/objs/opencv_java/opencv_java.o: jni/opencv_java.cpp \
+ C:/Users/LIAM/Desktop/Android/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h
+
+C:/Users/LIAM/Desktop/Android/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h:
