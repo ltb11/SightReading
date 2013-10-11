97d66099aeed3950093dd3eb4a2868f4ebbc41b0
liambarden <liamofbarden@googlemail.com>
2013-10-11 15:12:20 +0100

    may need to commit this

---
+++ SightReading/jni/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := SightReading
+LOCAL_SRC_FILES := SightReading.cpp
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ SightReading/obj/local/armeabi/objs/SightReading/SightReading.o.d
@@ -0,0 +1,4 @@
+obj/local/armeabi/objs/SightReading/SightReading.o: jni/SightReading.cpp \
+ C:/Users/LIAM/Desktop/Android/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h
+
+C:/Users/LIAM/Desktop/Android/android-ndk-r9/platforms/android-9/arch-arm/usr/include/jni.h:
