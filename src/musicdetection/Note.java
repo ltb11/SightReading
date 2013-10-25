49425cf0586031de6f02224760217c32c41c581b
Guillaume Lacroix <gel11@ic.ac.uk>
2013-10-25 14:39:47 +0100

    Started GUI implementation

--- src/musicdetection/Note.java
+++ src/musicdetection/Note.java
@@ -5,25 +5,12 @@ import org.opencv.core.Point;
 public class Note {
 
 	private Point center;
-	private double duration;
 	
-	public Note(Point center, double duration) {
-		this.center = center;
-		this.duration = duration;
-	}
-	
-	// Only for debugging purposes
 	public Note(Point center) {
 		this.center = center;
-		this.duration = 1.0;
 	}
 	
 	public Point center() {
 		return center;
 	}
-	
-	public double duration() {
-		return duration;
-	}
-	
 }
--- src/org/sightreading/SightReadingActivity.java
+++ src/org/sightreading/SightReadingActivity.java
@@ -1,5 +1,6 @@
 package org.sightreading;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,18 +12,17 @@ import musicdetection.Line;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import utils.Utils;
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class SightReadingActivity extends Activity {
 	public static final String TAG = "SightReadingActivity";
@@ -57,7 +57,15 @@ public class SightReadingActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		Log.i(TAG, "called onCreate");
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		Button button = new Button(this);
+		button.setWidth(100);
+		button.setHeight(100);
+        button.setText("I'm a motherfucking button!");
+        RelativeLayout l = new RelativeLayout(this);
+        l.addView(button, 200, 200);
+        setContentView(l);
+        
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this,
@@ -79,14 +87,14 @@ public class SightReadingActivity extends Activity {
 		LinkedList<Integer> divisions = Utils.detectDivisions(proj, 190);
 		List<Mat> result = Utils.cut(sheet, divisions);
 		List<Mat> actualResult = new LinkedList<Mat>();
-		Map<Mat, List<Line>> map = new HashMap<Mat, List<Line>>();
+		Map<Mat, List<Line>> staveMap = new HashMap<Mat, List<Line>>();
 		for (Mat m : result) {
 			Mat clone = m.clone();
 			Utils.invertColors(m);
 			Mat lines = new Mat();
 			Imgproc.HoughLinesP(m, lines, 1, Math.PI / 180, 100);
 			Imgproc.cvtColor(clone, clone, Imgproc.COLOR_GRAY2BGR);
-			map.add
+			staveMap.put(clone, Utils.getHoughLinesFromMat(lines));
 			actualResult.add(clone);
 		}
 		Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2BGR);
