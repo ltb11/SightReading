94e2d6a70f17cb5daddea69dd9089edfce820e5d
Guillaume Lacroix <gel11@ic.ac.uk>
2013-10-26 02:35:24 +0100

    Graphic and coding design improvements on the GUI. EditText weirdly not removing cursor when exited. To fix

--- src/org/sightreading/SightReadingActivity.java
+++ src/org/sightreading/SightReadingActivity.java
@@ -22,21 +22,30 @@ import org.opencv.imgproc.Imgproc;
 import utils.SheetStrip;
 import utils.Utils;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SightReadingActivity extends Activity {
 	public static final String TAG = "SightReadingActivity";
+	public static EditText currentFileName;
 
 	private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
 		@Override
@@ -109,12 +118,37 @@ public class SightReadingActivity extends Activity {
 			}
 		});
 		
-		EditText currentFileName = (EditText) findViewById(R.id.filePath);
-		currentFileName.setOnClickListener(new View.OnClickListener() {
+		currentFileName = (EditText) findViewById(R.id.filePath);
+		currentFileName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+	        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+	            if (actionId == KeyEvent.KEYCODE_ENTER) {
+	                // hide virtual keyboard
+	                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+	                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+	                currentFileName.setCursorVisible(false);
+	                return true;
+	            }
+	            return false;
+	        }
+	    });
+		
+		LinearLayout l = (LinearLayout) findViewById(R.id.globalLayout);
+		l.setOnTouchListener(new OnTouchListener() {
 			
 			@Override
-			public void onClick(View v) {
-			//TODO: implement this method and all similar ones (onkey and all that shit)	
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+		            if (currentFileName.isFocused()) {
+		                Rect outRect = new Rect();
+		                currentFileName.getGlobalVisibleRect(outRect);
+		                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+		                    currentFileName.clearFocus();
+		                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE); 
+		                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+		                }
+		            }
+		        }
+		        return false;
 			}
 		});
 		
