bba5e907c7d710026b806f4797daaedb921a79b2
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2013-10-28 19:27:54 +0000

    added assets and now there's a crotchet rest at the start

--- src/midiconversion/Converter.java
+++ src/midiconversion/Converter.java
@@ -30,7 +30,7 @@ public class Converter{
         tempoTrack.insertEvent(t);
           
         double crotchetLength = 60000.0 / (bpm * PPQ);
-        int nextNote = 0;
+        int nextNote = crotchetLength;
         for(Bar bar : piece){
             for(Chord chord: bar){
                 int channel = 0; 
