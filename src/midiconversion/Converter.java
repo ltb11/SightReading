daed1835c7fbd1e1e643b7f24df645acb7999050
Will de Renzy-Martin <wrd11@doc.ic.ac.uk>
2013-10-28 13:03:49 +0000

    fixed loss of accuracy

--- src/midiconversion/Converter.java
+++ src/midiconversion/Converter.java
@@ -19,7 +19,8 @@ public class Converter{
     public static MidiFile Convert(Piece piece){
         MidiTrack tempoTrack = new MidiTrack();
         MidiTrack noteTrack  = new MidiTrack();
-    
+        int PPQ = 96;
+
         TimeSignature ts = new TimeSignature();
         Tempo t = new Tempo();
         int bpm = piece.getBpm();
@@ -29,15 +30,15 @@ public class Converter{
         tempoTrack.insertEvent(t);
         
           
-        double crotchetLength = 60.0 / bpm;
+        double crotchetLength = 60000.0 / (bpm * PPQ);
         int nextNote = 0;
         for(Bar bar : piece){
             for(Chord chord: bar){
                 int channel = 0; 
                 for(Note note: chord){
-                    double duration = note.getDuration();
+                    int duration = note.getDuration();
                     duration /= Note.CROTCHET_DURATION;
-                    double length = crotchetLength * duration;
+                    int length = (int) crotchetLength * duration;
                     noteTrack.insertNote(channel,note.getPitch(),note.getVelocity(), nextNote,length);
                     channel++; 
                 }
