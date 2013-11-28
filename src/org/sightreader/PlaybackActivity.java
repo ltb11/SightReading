d9b196c1081cf2a51d6bcf7ad31b067b3d09c7a3
liambarden <liamofbarden@googlemail.com>
2013-11-28 17:01:25 +0000

    working with test images

--- src/org/sightreader/PlaybackActivity.java
+++ src/org/sightreader/PlaybackActivity.java
@@ -1,14 +1,6 @@
 package org.sightreader;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-
 import playback.Playback;
-import utils.OurUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
